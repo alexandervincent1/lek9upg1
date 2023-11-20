c6341437594acf558445752762ab43ec0466c917
alexandervincent1 <alexander.vincent@elev.ga.ntig.se>
2023-11-20 09:28:01 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    }
+    static double cirkel (double radie)
+    {
+        radie=9;
+        double omkrets = radie*2;
+        omkrets=radie*3.14;
+        System.out.println(omkrets);
+        return omkrets;
+    }
+}
